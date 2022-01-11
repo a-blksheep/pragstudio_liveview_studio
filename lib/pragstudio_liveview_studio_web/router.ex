e7c4edc311e743eff3969d862d840fa82a556de9
Jason Lamb <blksheep@tutanota.com>
2022-01-11 20:18:56 +0200

    Added some additional files

--- lib/pragstudio_liveview_studio_web/router.ex
+++ lib/pragstudio_liveview_studio_web/router.ex
@@ -17,7 +17,7 @@ defmodule PragstudioLiveviewStudioWeb.Router do
   scope "/", PragstudioLiveviewStudioWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", PageLive
   end
 
   # Other scopes may use custom stacks.
