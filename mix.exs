afea293d9420657a806f1c69e35ef2f11065a76a
Jason Lamb <blksheep@tutanota.com>
2022-01-12 23:10:14 +0200

    Completed first additional excercise

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule PragstudioLiveviewStudio.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:faker, "~> 0.17.0"},
-      {:number, "~> 1.0"}
+      {:number, "~> 1.0"},
+      {:timex, "~> 3.0"}
     ]
   end
 
