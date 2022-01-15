db57c4667a5acdfee385d187bd398ab5e157ecb3
Jason Lamb <blksheep@tutanota.com>
2022-01-15 13:12:57 +0200

    Additional exercise #1

--- lib/pragstudio_liveview_studio_web/live/license_live.ex
+++ lib/pragstudio_liveview_studio_web/live/license_live.ex
@@ -47,7 +47,7 @@ defmodule PragstudioLiveviewStudioWeb.LicenseLive do
             </span>
           </div>
           <form phx-change="update">
-            <input type="range" min="1" max="10" name="seats" value={ @seats } />
+            <input type="range" min="1" max="10" name="seats" value={ @seats } phx-debounce="250" />
           </form>
           <div class="amount">
             <%= number_to_currency(@amount) %>
